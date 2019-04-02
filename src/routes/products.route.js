7032f9699baa09e7957cf597901cdff9f0ad078a
JohnSnow18 <jhonjajoy1803@gmail.com>
2019-04-01 22:45:37 -0500

    API_rest

--- src/routes/products.route.js
+++ src/routes/products.route.js
@@ -4,19 +4,66 @@ const db = require('../db.js')
 const slug = require('slug-generator')
 
 router.get('/', async (req, res) => {
-   const response = await db.select().from('products')
-   res.json(response)
+   /*   const response = await db.select().from('products') */
+   const products = db.select().from('products')
+   const query = req.query
+   const per_page = (query.per_page || 10) * 1 //mostrar de 10
+   const page = (query.page || 1) * 1 //per_page que pagina esta navegando
+
+   products.limit(per_page).offset((page-1)* per_page)
+
+   const data = {
+      per_page : per_page ,
+      page: page ,
+      data : await products
+   }
+
+   res.json(data)
 
 })
 
-router.post('/', async(req, res)=>{
+router.post('/', async (req, res) => {
    let data = req.body //se llama body donde en donde esta guardado todo
    data.slug = slug(data.name)
    let product = await db('products').insert(data)
 
-  /*  db('products').insert(data)  //insert() es una funcion */
+   /*  db('products').insert(data)  //insert() es una funcion */
 
-  res.json('Guardado')
+   res.json('Guardado')
 })
+
+router.delete('/:id', async (req, res) => {
+   console.log(req.params)
+
+   if (!req.params.id) {
+      res.send('el id es requerido')
+      return false
+   }
+   await db('products').where('id', req.params.id).del()
+
+
+   res.send("eliminando")
+})
+
+router.put('/:id', async (req, res) => {
+   //en el put utilizamos el params y body
+   const { id } = req.params
+
+   let product = await db.select().from('products').where('id', id)
+
+   if (!product.length) {
+      res.send('el producto no existe')
+      return false
+   }
+
+   let data = req.body
+   data.slug = slug(data.name)
+
+   await db('products').where('id', id).update(data)
+
+   res.send("actualizado")
+
+})
+
 
 module.exports = router
